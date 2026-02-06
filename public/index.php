026b0568c2fd867adf988be6d937e835b814da37
CSSkill.com <support@csskill.com>
2026-02-06 11:23:26 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,18 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-set_time_limit(60);
-error_reporting(E_ALL);
-
-// Configure session for HTTPS
-session_set_cookie_params([
-    'lifetime' => 604800, // 7 days
-    'path' => '/',
-    'domain' => '', // Let browser determine domain
-    'secure' => true, // HTTPS only
-    'httponly' => true, // No JavaScript access
-    'samesite' => 'Lax' // CSRF protection
-]);
 
 session_start();
 
